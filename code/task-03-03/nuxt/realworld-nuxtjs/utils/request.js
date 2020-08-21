ca6c3cd20949e4202d27d3922bdf506f21d0b497
chenfei <1251541611@qq.com>
2020-08-21 18:07:01 +0800

    axios 拦截器

--- code/task-03-03/nuxt/realworld-nuxtjs/utils/request.js
+++ code/task-03-03/nuxt/realworld-nuxtjs/utils/request.js
@@ -5,7 +5,15 @@ const request = axios.create({
 });
 
 // 请求拦截器
-
+request.interceptors.request.use(
+  function (config) {
+    return config;
+  },
+  function (error) {
+    // 请求失败（此时请求还未发出去）
+    return Promise.reject(error);
+  }
+);
 
 // 响应拦截器
 
