8cf8f166f764ec958116aa17e424933f08f6ec92
米娅莫拉苏娜丹妮谢莉红 <1251541611@qq.com>
2020-09-18 18:01:00 +0800

    笔记

---
+++ note/linux.md
@@ -0,0 +1,5 @@
+# linux
+
+* [centos安装mysql, 修改密码等](https://blog.csdn.net/kabolee/article/details/82528913?utm_medium=distribute.pc_relevant.none-task-blog-BlogCommendFromMachineLearnPai2-1.channel_param&depth_1-utm_source=distribute.pc_relevant.none-task-blog-BlogCommendFromMachineLearnPai2-1.channel_param)
+
+* [linux 修改创建数据库](https://www.cnblogs.com/donqiang/articles/2057972.html)
--- task/ks.js
+++ task/ks.js
@@ -8,7 +8,6 @@
 //   console.log('yellow')
 // } // 2秒
 
-
 // function promise(fn) {
 //     return  new Promise((resolve, reject) => {
 //         fn(resolve)
@@ -17,18 +16,18 @@
 // function light() {
 //     let lightOn = promise((resolve) => {
 //         red();
-//         setTimeout(() => {  
+//         setTimeout(() => {
 //             resolve()
 //         }, 3000)
 //     }).then(() => {
 //         green();
 //         return promise((resolve) => {
-//             setTimeout(() => {  
+//             setTimeout(() => {
 //                 resolve()
 //             }, 1000)
 //         })
 //     }).then(() => {
-//         yellow(); 
+//         yellow();
 //         setTimeout(() => {
 //           light()
 //         }, 2000)
@@ -51,3 +50,45 @@
 // // })
 // console.log("result 1",User.action.getCount())
 // console.log("result 2",getCount())
+
+// var permuteUnique = function (nums) {
+//   let res = [];
+//   let len = nums.length;
+
+//   nums = nums.sort();
+
+//   console.log(nums, "nums");
+
+//   let dfs = (begin) => {
+//     if (begin === len - 1) {
+//       res.push(nums.slice());
+//       console.log("sssss", nums);
+//       return false;
+//     }
+
+//    let map = new Map();
+
+//     for (let i = begin; i < len; i++) {
+//       if (i !== begin && (nums[begin] === nums[i] || nums[i-1]==nums[i])) {
+//         continue;
+//       } else {
+//         let s = ""+nums[begin]+""+nums[i];
+//         if(map.has(s)){
+//            continue;
+//         } else{
+//           map.set(s,1);
+//         }
+//         console.log(i, begin);
+//         [nums[begin], nums[i]] = [nums[i], nums[begin]];
+//         dfs(begin + 1);
+//         [nums[begin], nums[i]] = [nums[i], nums[begin]];
+//       }
+//     }
+//   };
+
+//   dfs(0);
+
+//   return res;
+// };
+
+// console.log(permuteUnique([2, 1, 1, 2]));
